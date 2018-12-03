1dacc6bc189c6b763ec5ab09a17a5b2b25adcaf3
fabian <fabian.makt@gmail.com>
2018-12-03 22:05:19 +0100

    actividad

---
+++ ejr5.rb
@@ -0,0 +1,4 @@
+meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']
+ventas = [2000, 3000, 1000, 5000, 4000]
+nuevo_hash = [meses , ventas].transpose.to_h.invert
+puts nuevo_hash.max_by {|key,value|key}
