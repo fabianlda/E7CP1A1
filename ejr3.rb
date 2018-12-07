92990e8123c243587b2f01c12504951fdf618f8a
fabian <fabian.makt@gmail.com>
2018-12-07 02:25:38 +0100

    actividad

--- ejr3.rb
+++ ejr3.rb
@@ -2,6 +2,6 @@ h = { "x": 1, "y": 2 }
 h [:z] = 5
 h [:x] = 5
 h.delete(:y)
-h.each { |key, value| puts 'yeaah' if key == :z }
+h.each { |key, _value| puts 'yeaah' if key == :z }
 h = h.invert
 print h
---
+++ ejr7.rb
@@ -0,0 +1,34 @@
+puts '***Elije una opcion***'
+puts 'opcion1*agregar'
+puts 'opcion2*eliminar'
+puts 'opcion3*actualizar'
+puts 'opcion4*stock total'
+puts 'opcion5*cantidad mas grande'
+puts 'opcion6*check'
+puts 'opcion7*salir'
+# agregar inventario #
+inventario = { Notebooks: 4, PC_Escritorio: 6, Routers: 10, Impresoras: 6 }
+opcion = gets.chomp.to_i
+
+if opcion == 1
+  loop do
+    print 'ingresa el nombre del producto'
+    inven = gets.chomp
+    print 'ingresa el valor del producto'
+    valor = gets.chomp.to_i
+    inventario[inven] = valor
+    print 'desea ingregar otro producto [s/7 para salir]'
+    opcion = gets.chomp
+    break if opcion == '7'
+  end
+end
+inventario = { Notebooks: 4, PC_Escritorio: 6, Routers: 10, Impresoras: 6 }
+if opcion == 2
+  puts inventario
+  loop do
+    print 'ingresa el producto a eliminar'
+    inventario.delete gets.chomp
+    print 'desea eliminar otro producto? [si/ 7 para salir]'
+    break if opcion == '7'
+  end
+end
